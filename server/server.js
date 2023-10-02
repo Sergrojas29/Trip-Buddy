8053a8c0af50e2df2178a869e2813418fa9e11fa
Timothy <TJSalamatin@gmail.com>
2023-10-02 09:08:17 -0400

    added path

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const { authMiddleware } = require('./utils/auth');
-
+const path = require('path');
 // Import the two parts of a GraphQL schema
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
