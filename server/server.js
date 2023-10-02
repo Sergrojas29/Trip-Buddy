3b7e0ad1bb715aa46d63dd98c74ece06165a3ef3
Timothy <TJSalamatin@gmail.com>
2023-10-02 09:04:53 -0400

    updated server

--- server/server.js
+++ server/server.js
@@ -18,14 +18,21 @@ const app = express();
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
   await server.start();
-  
+
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
-  
+
   app.use('/graphql', expressMiddleware(server, {
     context: authMiddleware
   }));
-  ;
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
 
   db.once('open', () => {
     app.listen(PORT, () => {
@@ -34,6 +41,7 @@ const startApolloServer = async () => {
     });
   });
 };
+
 
 // Call the async function to start the server
 startApolloServer();
